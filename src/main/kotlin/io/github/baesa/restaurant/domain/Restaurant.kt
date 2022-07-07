8d0cad4cdcee70c5505f536ed6a16d5af2265629
comstering <coms1101@naver.com>
2022-07-07 22:44:35 +0900

    ✨ feat: Add domain is restaurant

---
+++ src/main/kotlin/io/github/baesa/restaurant/domain/Restaurant.kt
@@ -0,0 +1,37 @@
+package io.github.baesa.restaurant.domain
+
+import lombok.Data
+import org.hibernate.annotations.ColumnDefault
+import org.hibernate.annotations.DynamicInsert
+import org.hibernate.annotations.DynamicUpdate
+import javax.persistence.Column
+import javax.persistence.Entity
+import javax.persistence.GeneratedValue
+import javax.persistence.GenerationType
+import javax.persistence.Id
+import javax.persistence.Table
+
+@Entity
+@Data
+@DynamicInsert
+@DynamicUpdate
+@Table(name = "restaurant")
+open class Restaurant {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id", nullable = false)
+    open var id: Long? = null
+
+    @Column(name = "name", nullable = false)
+    open var name: String? = null
+
+    @Column(name = "address", nullable = false)
+    open var address: String? = null
+
+    @Column(name = "telephone_number", nullable = false)
+    open var telephoneNumber: String? = null
+
+    @Column(name = "rating", nullable = false)
+    @ColumnDefault("0.0")
+    open var rating: Float? = null
+}
