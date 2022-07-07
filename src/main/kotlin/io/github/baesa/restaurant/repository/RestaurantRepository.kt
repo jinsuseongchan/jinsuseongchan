e79747029a949af00a56c8e109f721659923c01c
comstering <coms1101@naver.com>
2022-07-07 23:50:10 +0900

    ✨ feat: Add JPA Repository function
    Restaurant find by category id

--- src/main/kotlin/io/github/baesa/restaurant/repository/RestaurantRepository.kt
+++ src/main/kotlin/io/github/baesa/restaurant/repository/RestaurantRepository.kt
@@ -1,7 +1,9 @@
 package io.github.baesa.restaurant.repository;
 
+import io.github.baesa.restaurant.domain.Category
 import io.github.baesa.restaurant.domain.Restaurant
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface RestaurantRepository : JpaRepository<Restaurant, Long> {
+    fun findByCategoryId(categoryId: Category): List<Restaurant>
 }
