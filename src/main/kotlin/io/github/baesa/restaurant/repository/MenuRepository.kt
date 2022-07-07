4fd0e9f4bec9192e0ee057b377349abe8959fcc2
comstering <coms1101@naver.com>
2022-07-07 22:56:37 +0900

    ✨ feat: Add JPA Repository function
    Menu find by restaurant id

--- src/main/kotlin/io/github/baesa/restaurant/repository/MenuRepository.kt
+++ src/main/kotlin/io/github/baesa/restaurant/repository/MenuRepository.kt
@@ -1,7 +1,9 @@
 package io.github.baesa.restaurant.repository;
 
 import io.github.baesa.restaurant.domain.Menu
+import io.github.baesa.restaurant.domain.Restaurant
 import org.springframework.data.jpa.repository.JpaRepository
 
 interface MenuRepository : JpaRepository<Menu, Long> {
+    fun findByRestaurantId(restaurantId: Restaurant): List<Menu>
 }
