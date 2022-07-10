bf10241a5a5c7ad255a5acaeefda486e4760a5c9
comstering <coms1101@naver.com>
2022-07-10 23:33:06 +0900

    ➕ build: Add kotlin-spring plugin
    Add all open at Entity annotation

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,9 +3,14 @@ import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 plugins {
     id("org.springframework.boot") version "2.7.1"
     id("io.spring.dependency-management") version "1.0.11.RELEASE"
+    id("org.jetbrains.kotlin.plugin.spring") version "1.6.21"
     kotlin("jvm") version "1.6.21"
     kotlin("plugin.spring") version "1.6.21"
     kotlin("plugin.jpa") version "1.6.21"
+}
+
+allOpen {
+    annotation("javax.persistence.Entity")
 }
 
 group = "io.github"
