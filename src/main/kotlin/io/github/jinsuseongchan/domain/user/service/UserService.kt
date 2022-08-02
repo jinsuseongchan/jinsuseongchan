68a03562d98f684ff9b9eeeb152da112a72f5f0f
comstering <coms1101@naver.com>
2022-08-03 01:57:46 +0900

    ✨ feat: Implement user login function

--- src/main/kotlin/io/github/jinsuseongchan/domain/user/service/UserService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/user/service/UserService.kt
@@ -1,13 +1,20 @@
 package io.github.jinsuseongchan.domain.user.service
 
+import io.github.jinsuseongchan.domain.user.domain.User
 import io.github.jinsuseongchan.domain.user.repository.UserRepository
+import io.github.jinsuseongchan.global.config.auth.UserDetailImpl
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.security.core.userdetails.UserDetails
 import org.springframework.security.core.userdetails.UserDetailsService
+import org.springframework.security.core.userdetails.UsernameNotFoundException
+import org.springframework.security.crypto.bcrypt.BCryptPasswordEncoder
 import org.springframework.stereotype.Service
 
 @Service
 class UserService(private val userRepository: UserRepository): UserDetailsService {
-    override fun loadUserByUsername(username: String?): UserDetails {
-        TODO("Not yet implemented")
+    @Throws(UsernameNotFoundException::class)
+    override fun loadUserByUsername(username: String): UserDetails {
+        val user: User = userRepository.findByUsername(username)?: throw UsernameNotFoundException("아이디나 비밀번호가 일치하지 않습니다.")
+        return UserDetailImpl(user)
     }
 }
