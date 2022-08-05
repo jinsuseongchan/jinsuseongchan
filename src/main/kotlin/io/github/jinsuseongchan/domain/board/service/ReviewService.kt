9c5d9a220bdb1558c4ad3bc15c1ece6791f5cbaf
comstering <coms1101@naver.com>
2022-08-06 00:07:36 +0900

    ✨ feat: Add Dependency Injection

--- src/main/kotlin/io/github/jinsuseongchan/domain/board/service/ReviewService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/board/service/ReviewService.kt
@@ -1,8 +1,9 @@
 package io.github.jinsuseongchan.domain.board.service
 
+import io.github.jinsuseongchan.domain.board.repository.ReviewRepository
 import org.springframework.stereotype.Service
 
 @Service
-class ReviewService(val ) {
+class ReviewService(val reviewRepository: ReviewRepository) {
 
 }
