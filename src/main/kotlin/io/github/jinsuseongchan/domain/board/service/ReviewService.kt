a180ae12fdcc9e1ed6b3851f7167e3157045abf8
comstering <coms1101@naver.com>
2022-08-06 00:11:22 +0900

    ✨ feat: Create function
    User can create review

--- src/main/kotlin/io/github/jinsuseongchan/domain/board/service/ReviewService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/board/service/ReviewService.kt
@@ -1,9 +1,13 @@
 package io.github.jinsuseongchan.domain.board.service
 
+import io.github.jinsuseongchan.domain.board.domain.Review
 import io.github.jinsuseongchan.domain.board.repository.ReviewRepository
 import org.springframework.stereotype.Service
 
 @Service
 class ReviewService(val reviewRepository: ReviewRepository) {
 
+    fun createReview(review: Review): Review {
+        return reviewRepository.save(review)
+    }
 }
