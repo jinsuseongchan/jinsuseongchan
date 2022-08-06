be0a4d4eee90685190da083b64802966eac68476
comstering <coms1101@naver.com>
2022-08-06 13:19:18 +0900

    ✨ feat: Create get reviews by restaurant

--- src/main/kotlin/io/github/jinsuseongchan/domain/board/service/ReviewService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/board/service/ReviewService.kt
@@ -2,6 +2,9 @@ package io.github.jinsuseongchan.domain.board.service
 
 import io.github.jinsuseongchan.domain.board.domain.Review
 import io.github.jinsuseongchan.domain.board.repository.ReviewRepository
+import io.github.jinsuseongchan.domain.restaurant.domain.Restaurant
+import io.github.jinsuseongchan.domain.restaurant.repository.RestaurantRepository
+import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,5 +12,13 @@ class ReviewService(val reviewRepository: ReviewRepository) {
 
     fun createReview(review: Review): Review {
         return reviewRepository.save(review)
+    }
+
+    @Autowired
+    lateinit var restaurantRepository: RestaurantRepository
+
+    fun getReviewsByRestaurant(restaurantId: Long): List<Review> {
+        val restaurant: Restaurant = restaurantRepository.findById(restaurantId).get()
+        return reviewRepository.findByRestaurantId(restaurant)
     }
 }
