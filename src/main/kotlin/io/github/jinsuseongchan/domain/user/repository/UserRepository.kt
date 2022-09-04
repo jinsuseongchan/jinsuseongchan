d4c7cb04bd7c5edea667a03cbc045b0d0c35c613
comstering <coms1101@naver.com>
2022-09-04 17:54:03 +0900

    🔥 revert: remove about User domain

--- src/main/kotlin/io/github/jinsuseongchan/domain/user/repository/UserRepository.kt
+++
@@ -1,8 +0,0 @@
-package io.github.jinsuseongchan.domain.user.repository;
-
-import io.github.jinsuseongchan.domain.user.domain.User
-import org.springframework.data.jpa.repository.JpaRepository
-
-interface UserRepository : JpaRepository<User, Long> {
-    fun findByUsername(username: String): User?
-}
