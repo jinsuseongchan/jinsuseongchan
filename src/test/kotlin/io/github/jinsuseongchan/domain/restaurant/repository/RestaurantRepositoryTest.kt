fbc86106d970c760221dcf306667fce236d4f468
comstering <coms1101@naver.com>
2022-09-12 02:05:59 +0900

    ✅ test: add looking up restaurant info by restaurant id

--- src/test/kotlin/io/github/jinsuseongchan/domain/restaurant/repository/RestaurantRepositoryTest.kt
+++ src/test/kotlin/io/github/jinsuseongchan/domain/restaurant/repository/RestaurantRepositoryTest.kt
@@ -11,6 +11,7 @@ import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
 import org.springframework.dao.InvalidDataAccessApiUsageException
+import org.springframework.data.repository.findByIdOrNull
 
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
 @DataJpaTest(showSql = true)
@@ -19,10 +20,11 @@ class RestaurantRepositoryTest @Autowired constructor(
     val categoryRepository: CategoryRepository
 ) {
 
+    val category = Category(name = "테스트 카테고리")
+
     @Nested
     @DisplayName("식당 정보 저장 테스트")
     inner class RestaurantSaveTest {
-        val category = Category(name = "테스트 카테고리")
 
         @Test
         @DisplayName("식당 정보를 저장할 수 있다")
@@ -37,10 +39,10 @@ class RestaurantRepositoryTest @Autowired constructor(
             )
 
             // when
-            val savedRestaurant: Restaurant = restaurantRepository.save(restaurant)
+            val savedRestaurant = restaurantRepository.save(restaurant)
 
             // then
-            assertThat(restaurant).isSameAs(savedRestaurant)
+            assertThat(savedRestaurant).isSameAs(restaurant)
         }
 
         @Test
@@ -58,6 +60,33 @@ class RestaurantRepositoryTest @Autowired constructor(
             // then
             assertThatExceptionOfType(InvalidDataAccessApiUsageException::class.java)
                 .isThrownBy { restaurantRepository.save(restaurant) }
+        }
+    }
+
+    @Nested
+    @DisplayName("식당 정보 조회 테스트")
+    inner class RestaurantLookUpTest {
+
+        @Test
+        @DisplayName("식당 아이디로 식당 정보를 조회할 수 있다")
+        fun succeedLookingUpRestaurantInfo() {
+            //given
+            val testCategory = categoryRepository.save(category)
+            val savedRestaurant = restaurantRepository.save(
+                Restaurant(
+                    name = "식당 테스트",
+                    address = "테스트 주소",
+                    telephoneNumber = "0213234345",
+                    categoryId = testCategory
+                )
+            )
+            val savedRestaurantId = savedRestaurant.id?:1L
+
+            // when
+            val findRestaurant = restaurantRepository.findByIdOrNull(savedRestaurantId)
+
+            // then
+            assertThat(findRestaurant).isSameAs(savedRestaurant)
         }
     }
 }
