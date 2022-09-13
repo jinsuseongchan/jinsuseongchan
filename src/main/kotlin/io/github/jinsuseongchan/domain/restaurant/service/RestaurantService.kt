1800127cf1cf2a8d9c04e81bb1ad2e96fa7eb79b
comstering <coms1101@naver.com>
2022-09-13 23:30:46 +0900

    ✨ feat: Implement restaurant service getting restaurant by restaurant id

--- src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/RestaurantService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/RestaurantService.kt
@@ -2,13 +2,12 @@ package io.github.jinsuseongchan.domain.restaurant.service
 
 import io.github.jinsuseongchan.domain.restaurant.domain.Restaurant
 import io.github.jinsuseongchan.domain.restaurant.repository.RestaurantRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
-import java.util.Optional
 
 @Service
 class RestaurantService (val restaurantRepository: RestaurantRepository) {
-    fun loadRestaurantByRestaurantId(restaurantId: Long): Optional<Restaurant> {
-        val restaurant = restaurantRepository.findById(restaurantId)
-        return restaurant
+    fun getRestaurantByRestaurantId(restaurantId: Long): Restaurant {
+        return restaurantRepository.findByIdOrNull(restaurantId) ?: throw Exception("에러")
     }
 }
