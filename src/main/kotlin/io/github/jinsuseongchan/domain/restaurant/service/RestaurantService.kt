860a98b5f291de7ed89d7521213c9d75411731b2
comstering <coms1101@naver.com>
2022-09-15 22:55:55 +0900

    ✨ feat: Add restaurant save service

--- src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/RestaurantService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/RestaurantService.kt
@@ -6,7 +6,12 @@ import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class RestaurantService (val restaurantRepository: RestaurantRepository) {
+class RestaurantService(val restaurantRepository: RestaurantRepository) {
+
+    fun saveRestaurant(restaurant: Restaurant): Restaurant {
+        return restaurantRepository.save(restaurant)
+    }
+
     fun getRestaurantByRestaurantId(restaurantId: Long): Restaurant {
         return restaurantRepository.findByIdOrNull(restaurantId) ?: throw Exception("에러")
     }
