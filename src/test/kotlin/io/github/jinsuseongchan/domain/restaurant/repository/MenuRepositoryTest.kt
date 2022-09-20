7d8949adf99380df31e93d7b302361ac04a59328
comstering <coms1101@naver.com>
2022-09-20 23:15:20 +0900

    🧪 test: Add menu save fail test

--- src/test/kotlin/io/github/jinsuseongchan/domain/restaurant/repository/MenuRepositoryTest.kt
+++ src/test/kotlin/io/github/jinsuseongchan/domain/restaurant/repository/MenuRepositoryTest.kt
@@ -5,12 +5,14 @@ import io.github.jinsuseongchan.domain.restaurant.domain.Menu
 import io.github.jinsuseongchan.domain.restaurant.domain.Money
 import io.github.jinsuseongchan.domain.restaurant.domain.Restaurant
 import org.assertj.core.api.Assertions.assertThat
+import org.assertj.core.api.Assertions.assertThatExceptionOfType
 import org.junit.jupiter.api.DisplayName
 import org.junit.jupiter.api.Nested
 import org.junit.jupiter.api.Test
 import org.springframework.beans.factory.annotation.Autowired
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest
+import org.springframework.dao.InvalidDataAccessApiUsageException
 
 @DataJpaTest(showSql = true)
 @AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.ANY)
@@ -48,6 +50,27 @@ class MenuRepositoryTest @Autowired constructor(
 
             // then
             assertThat(savedMenu).isSameAs(menu)
+        }
+
+        @Test
+        @DisplayName("잘못된 식당으로는 메뉴 정보를 저장할 수 없다")
+        fun failSavingCauseByNotExistRestaurant() {
+            // given
+            val menu = Menu(
+                name = "테스트 메뉴",
+                price = Money(money = 10000),
+                restaurantId = Restaurant(
+                    name = "저장되지 않은 식당",
+                    address = "주소",
+                    telephoneNumber = "021113333",
+                    categoryId = Category(name = "저장되지 않는 카테고리")
+                )
+            )
+
+            // when
+            // then
+            assertThatExceptionOfType(InvalidDataAccessApiUsageException::class.java)
+                .isThrownBy { menuRepository.save(menu) }
         }
     }
 }
