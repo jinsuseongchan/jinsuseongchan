7166d944603a1cb7e4c672ddc45ad44da1aeed76
comstering <coms1101@naver.com>
2022-09-23 21:53:28 +0900

    ✨ feat: Add get menu service by id

--- src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/MenuService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/MenuService.kt
@@ -2,6 +2,7 @@ package io.github.jinsuseongchan.domain.restaurant.service
 
 import io.github.jinsuseongchan.domain.restaurant.domain.Menu
 import io.github.jinsuseongchan.domain.restaurant.repository.MenuRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,5 +10,9 @@ class MenuService(val menuRepository: MenuRepository) {
 
     fun saveMenu(menu: Menu): Menu {
         return menuRepository.save(menu)
+    }
+
+    fun getMenuByMenuId(menuId: Long): Menu {
+        return menuRepository.findByIdOrNull(menuId) ?: throw Exception("메뉴가 존재하지 않습니다.")
     }
 }
