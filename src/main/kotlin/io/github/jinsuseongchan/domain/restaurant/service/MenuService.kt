4d58ad0477b63b6aea13ee5bc5cf2b5eaabd142d
comstering <coms1101@naver.com>
2022-09-23 22:05:51 +0900

    ✨ feat: Add get menu list by restaurant

--- src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/MenuService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/MenuService.kt
@@ -2,11 +2,15 @@ package io.github.jinsuseongchan.domain.restaurant.service
 
 import io.github.jinsuseongchan.domain.restaurant.domain.Menu
 import io.github.jinsuseongchan.domain.restaurant.repository.MenuRepository
+import io.github.jinsuseongchan.domain.restaurant.repository.RestaurantRepository
 import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
-class MenuService(val menuRepository: MenuRepository) {
+class MenuService(
+    val restaurantRepository: RestaurantRepository,
+    val menuRepository: MenuRepository
+) {
 
     fun saveMenu(menu: Menu): Menu {
         return menuRepository.save(menu)
@@ -14,5 +18,11 @@ class MenuService(val menuRepository: MenuRepository) {
 
     fun getMenuByMenuId(menuId: Long): Menu {
         return menuRepository.findByIdOrNull(menuId) ?: throw Exception("메뉴가 존재하지 않습니다.")
+    }
+
+    fun getMenuListByRestaurantId(restaurantId: Long): List<Menu> {
+        return menuRepository.findByRestaurantId(
+            restaurantId = restaurantRepository.findByIdOrNull(restaurantId) ?: throw Exception("식당이 존재하지 않습니다.")
+        )
     }
 }
