616845b47686f76b490a63b307d8e971e3c7d20d
comstering <coms1101@naver.com>
2022-09-27 09:30:55 +0900

    ✨ feat: Add get category by id service

--- src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/CategoryService.kt
+++ src/main/kotlin/io/github/jinsuseongchan/domain/restaurant/service/CategoryService.kt
@@ -2,6 +2,7 @@ package io.github.jinsuseongchan.domain.restaurant.service
 
 import io.github.jinsuseongchan.domain.restaurant.domain.Category
 import io.github.jinsuseongchan.domain.restaurant.repository.CategoryRepository
+import org.springframework.data.repository.findByIdOrNull
 import org.springframework.stereotype.Service
 
 @Service
@@ -9,6 +10,10 @@ class CategoryService(val categoryRepository: CategoryRepository) {
 
     fun saveCategory(category: Category): Category {
         return categoryRepository.save(category)
+    }
+
+    fun getCategoryByCategoryId(categoryId: Long): Category {
+        return categoryRepository.findByIdOrNull(categoryId) ?: throw Exception("카테고리 없음")
     }
 
     fun getAllCategory(): List<Category> {
